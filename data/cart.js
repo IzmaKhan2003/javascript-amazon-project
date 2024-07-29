25b9784ef7ac13ac98651c907dc8515498221d7a
IzmaKhan <izmakhan2003@gmail.com>
2024-07-29 08:36:14 -0700

    move addToCart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,24 @@
 export const cart = [];
+
+export function addToCart(productId){
+    let matchingcartItem;
+        cart.forEach((cartItem) => {
+            if (productId === cartItem.productId){
+                matchingcartItem = cartItem;
+
+                }
+        });
+        
+        const selectedQuantity = document.querySelector(`.js-quantity-selected-${productId}`).value;
+        const quantity = Number(selectedQuantity);
+
+
+        if(matchingcartItem){
+            matchingcartItem.quantity += quantity;
+        }else{
+        cart.push({
+            productId: productId,
+            quantity: quantity
+        });
+    }
+}
