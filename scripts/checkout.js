821f640683129eaeb147c86ecb4d96c4f30dc9ba
IzmaKhan <izmakhan2003@gmail.com>
2024-07-31 02:10:02 -0700

    delete product from cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js'; 
 import { formatCurrency } from './utils/money.js';
 
@@ -38,7 +38,7 @@ cart.forEach((cartItem) =>{
                     <span class="update-quantity-link link-primary">
                         Update
                     </span>
-                    <span class="delete-quantity-link link-primary">
+                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                         Delete
                     </span>
                     </div>
@@ -77,7 +77,7 @@ cart.forEach((cartItem) =>{
                     <div class="delivery-option">
                     <input type="radio"
                         class="delivery-option-input"
-                        name="delivery-option-${ssmatchingProduct.id}">
+                        name="delivery-option-${matchingProduct.id}">
                     <div>
                         <div class="delivery-option-date">
                         Monday, June 13
@@ -97,4 +97,10 @@ cart.forEach((cartItem) =>{
 document.querySelector(`.js-order-summary`)
 .innerHTML = cartSummaryHTML;
 
-console.log(cartSummaryHTML);
+document.querySelectorAll('.js-delete-link')
+.forEach((link) => {
+    link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+    });
+});
